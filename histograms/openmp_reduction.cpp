78f4e2f21482521c774820121ae046d0c41e438c
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2024-10-23 02:27:43 -0500

    fix BM_openMP_reduction

--- histograms/openmp_reduction.cpp
+++ histograms/openmp_reduction.cpp
@@ -3,10 +3,9 @@
 std::vector<int> OpenmpReduction::calculate(const int* random_input,
                                             const int maximo_valor,
                                             const int numero_elementos) {
-  std::vector<int> histograma(maximo_valor, 0);
+  int histograma[maximo_valor] = {0};
 #pragma omp parallel for reduction(+ : histograma[ : maximo_valor])
   for(int idx = 0; idx < numero_elementos; idx++) {
     histograma[random_input[idx] - 1]++;
   }
-  return histograma;
 }
